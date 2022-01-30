6fabc4b22373b1cf85618a32acb560016c97d657
AndrxsAF <andrxsaf@gmail.com>
2022-01-30 08:14:31 +0000

    featuring: initial visual

--- src/js/component/CardsPokemon.jsx
+++ src/js/component/CardsPokemon.jsx
@@ -1,22 +1,67 @@
 import PropTypes from "prop-types";
-import React from "react";
+import { getPokemon, getSpecificPokemon } from "../services/services.js";
+import React, { useEffect, useState } from "react";
 
 const CardsPokemon = (props) => {
+
+    const [pokemonList, setPokemonList] = useState({});
+
+    const capitalizeFirstLetter = (string) => string.charAt(0).toUpperCase() + string.slice(1)
+
+    const pokemonGetFunct = async (url) => {
+		try {
+			const res = await getPokemon(url);
+			const dataJSON = await res.json();
+			setPokemonList(dataJSON)
+		} catch (err) {
+			console.log(err);
+		}
+	};
+
+    const checkType = () => {
+        if (pokemonList.types) {
+            if (pokemonList.types.length > 1) {
+                return (
+                    <p className="card-text mb-1"><strong>Types:</strong> {capitalizeFirstLetter(pokemonList.types[0].type.name)} / {capitalizeFirstLetter(pokemonList.types[1].type.name)}.</p>
+                )
+            } else {
+                return (
+                    <p className="card-text mb-1"><strong>Type:</strong> {capitalizeFirstLetter(pokemonList.types[0].type.name)}.</p>
+                )
+            }
+        } else {
+            return null
+        }
+    }
+
+    useEffect(() => {
+        pokemonGetFunct(props.url)
+    }, [props.url])
+		
     return (
-        <div className="card" style={{width: "18rem"}}>
-            <img src="" className="card-img-top" alt={props.pokes}/>
-            <div className="card-body">
-                <h5 className="card-title">{props.pokes}</h5>
-                <p className="card-text">Type:</p>
-                <p className="card-text">: </p>
-                <a href="#" className="btn btn-primary">Read its pokedex description</a>
+        <div className="col-md-3 col-sm-12 p-4">
+            <div className="card">
+                <img src={pokemonList.sprites ? pokemonList.sprites.other["official-artwork"].front_default : null} className="card-img-top img-fluid" alt={props.pokes}/>
+                <div className="card-body bg-light">
+                    <h5 className="card-title fs-3 text-center">{capitalizeFirstLetter(props.pokes)}</h5>
+                     {checkType()}
+                    <p className="card-text mb-1"><strong>Height:</strong> {pokemonList.height} inches tall.</p>
+                    <p className="card-text"><strong>Weight:</strong> {pokemonList.weight} lbs.</p>
+                    <div className="container-fluid d-flex justify-content-between p-0">
+                        <a href="#" className="btn btn-primary d-flex justify-content-center align-items-center">Pokedex description</a>
+                        <input type="checkbox" className="btn-check" name={props.pokes} id={`${props.pokes}-check`}/>
+                        <label className="btn btn-outline-warning p-1" htmlFor={`${props.pokes}-check`}><img src="https://img.icons8.com/color/40/000000/star-pokemon.png"/></label>
+                    </div>
+                </div>
             </div>
         </div>
+        
     )
 }
 
 CardsPokemon.propTypes = {
-    pokes: PropTypes.string
+    pokes: PropTypes.string,
+    url: PropTypes.string
 }
 
 export default CardsPokemon
--- src/js/views/home.jsx
+++ src/js/views/home.jsx
@@ -1,16 +1,35 @@
 import React, { useEffect, useState } from "react";
+import PropTypes from "prop-types";
 import "../../styles/home.css";
 import CardsPokemon from "../component/CardsPokemon.jsx";
+import CardsRegion from "../component/CardsRegion.jsx";
 import { getPokemon, getSpecificPokemon } from "../services/services.js";
+const regionUrl = "https://pokeapi.co/api/v2/version-group/"
 
-const mainUrl = "https://pokeapi.co/api/v2/pokemon/"
-let list = []
-
-export const Home = () => {
+export const Home = (props) => {
+	
+	const [urlState, setUrl] = useState("https://pokeapi.co/api/v2/pokemon/")
 	const [pokemon, setPokemon] = useState([]);
 	const [nextPage, setNextPage] = useState("");
 	const [previousPage, setPreviousPage] = useState(null);
-	const [pokemonList, setPokemonList] = useState([]);
+	const [pokedexNumbers, setPokedexNumbers] = useState([1, 20])
+	const [region, setRegion] = useState([])
+	const [nextButton, setNextButton] = useState(false);
+	const [previousButton, setPreviousButton] = useState(false);
+
+	const buttonNextPage = () => {
+		setNextButton(true)
+		setPokedexNumbers(pokedexNumbers.map((numbers) => numbers + 20))
+		console.log(pokedexNumbers)
+		setUrl(nextPage)
+	}
+	
+	const buttonPreviousPage = () => {
+		setPreviousButton(true)
+		setPokedexNumbers(pokedexNumbers.map((numbers) => numbers - 20))
+		console.log(pokedexNumbers)
+		setUrl(previousPage)
+	}
 
 	const pokemonGetFunct = async (url) => {
 		try {
@@ -19,44 +38,58 @@ export const Home = () => {
 			setPokemon(dataJSON.results)
 			setNextPage(dataJSON.next)
 			setPreviousPage(dataJSON.previous)
+			setPreviousButton(false)
+			setNextButton(false)
 		} catch (err) {
 			console.log(err);
 		}
 	};
 
-	const pokemonGetData = async (url) => {
+	const regionGetFunct = async (url) => {
 		try {
 			const res = await getPokemon(url);
 			const dataJSON = await res.json();
-			list.push(dataJSON)
+			setRegion(dataJSON.results)
 		} catch (err) {
 			console.log(err);
 		}
 	};
-
+		
 	useEffect(() => {
-		pokemonGetFunct(mainUrl);
-		list = []
-		pokemon.forEach((pokes) => {
-			getPokemon(pokes.url)
-			.then((response) => response.json())
-			.then((responseJson) => {
-				list.push(responseJson)
-			})
-			.catch((err) => console.log(err));
-		})
-		setPokemonList(list)
-	}, [nextPage]);
+		pokemonGetFunct(urlState);
+	}, [urlState]);
 
-	console.log(pokemonList[2])
+	regionGetFunct(regionUrl)
 
 	return (
-		<div className="text-center mt-5">
-			{pokemon.length > 0
+		<div className="mt-3">
+			<p className="ps-5 fs-1 m-0">Pokémon:</p>
+			<div className="m-0 d-flex overflow-auto">
+				{pokemon.length > 0
 				? pokemon.map((poke, index) => (
-						<CardsPokemon key={index} pokes={poke.name} />
+						<CardsPokemon key={index} pokes={poke.name} url={poke.url} />
 				  ))
 				: null}
+			</div>
+			<nav className="mt-4 d-flex justify-content-center" aria-label="Page navigation example">
+				<ul className="pagination">
+					<li className={pokedexNumbers[0] == 1 ? "page-item visually-hidden" : "page-item"}><button onClick={buttonPreviousPage} disabled={previousButton} className="page-link" href="#">{previousButton ? (<div className='spinner-border'/>) : "Previous"}</button></li>
+					<li className="page-item page-link text-muted">current Pokedex numbers: {`${pokedexNumbers[0]} - ${pokedexNumbers[1]}`}</li>
+					<li className={pokedexNumbers[1] == 900 ? "page-item visually-hidden" : "page-item"}><button onClick={buttonNextPage} className='page-link' disabled={nextButton} href='#'>{nextButton ? (<div className='spinner-border'/>) : "Next"}</button></li>
+				</ul>
+			</nav>
+			<p className="ps-5 fs-1 mt-3">Games:</p>
+			<div className="m-0 d-flex overflow-auto">
+				{region.length > 0
+				? region.map((region, index) => (
+						<CardsRegion key={index} region={region.name} url={region.url} />
+				  ))
+				: null}
+			</div>
 		</div>
 	);
 };
+
+Home.propTypes = {
+    
+}
